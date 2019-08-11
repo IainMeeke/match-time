2ba2556267a66de54cd4936f7f113e47c62d28c8
iainmeeke <meekei@tcd.iel>
2019-08-11 22:15:02 +0100

    validating period

--- src/main/scala/com/iain/timeConverter/TimeConverter.scala
+++ src/main/scala/com/iain/timeConverter/TimeConverter.scala
@@ -17,11 +17,40 @@ object TimeConverter extends LazyLogging {
     * @return
     */
   def validate(input: String): Boolean = {
-    input.split(" ")
+    val separate = input.split(" ")
+    val periodValidated: Either[String, String => Period] = separate.size match {
+      case invalidLength if invalidLength !=2 => Left("invalid")
+      case valid => validateShortPeriod(separate(0))
+    }
+
+//    val validateTime
+    periodValidated.map(per => per("hello"))
+  true
+  }
+
+
+  /**
+    * Validates that the provided short form period is the correct format. The provided
+    * period should be wrapped in square braces as `[period]`.
+    *
+    * If the period is valid then a Right of the constructor for that period is returned.
+    * If the period is invalid then a Left of a string is returned.
+    * @param period a short form period wrapped in square braces
+    * @return Either a left of error or a period constructor
+    */
+  def validateShortPeriod(period: String): Either[String, String => Period] = {
+    period match {
+      case "[PM]" => Right(preMatch)
+      case "[H1]" => Right(firstHalf)
+      case "[HT]" => Right(halfTime)
+      case "[H2]" => Right(secondHalf)
+      case "[FT]" => Right(fullTime)
+      case invalid => Left(s"$invalid is an invalid short form period")
+    }
   }
 }
 
-trait Period {
+sealed trait Period {
   val shortForm: String
   val longForm: String
 }
@@ -33,3 +62,16 @@ case class firstHalf(time: String) extends Period {
   override val shortForm: String = "H1"
   override val longForm: String = "FIRST_HALF"
 }
+case class halfTime(time: String) extends Period {
+  override val shortForm: String = "HT"
+  override val longForm: String = "HALF_TIME"
+}
+case class secondHalf(time: String) extends Period {
+  override val shortForm: String = "H2"
+  override val longForm: String = "SECOND_HALF"
+}
+case class fullTime(time: String) extends Period {
+  override val shortForm: String = "FT"
+  override val longForm: String = "FULL_TIME"
+}
+
--- src/test/scala/com/iain/timeConverter/TimeConverterTest.scala
+++ src/test/scala/com/iain/timeConverter/TimeConverterTest.scala
@@ -9,3 +9,33 @@ class TimeConverterTest extends FunSuite {
   }
 
 }
+
+class periodTest extends FunSuite {
+
+  test("Validate Short Period should return a Right(Period) for valid short form") {
+    def test(p: String, exp: String => Period): Unit = {
+      val actual = TimeConverter.validateShortPeriod(p)
+      assert(actual === Right(exp))
+    }
+    test("[PM]", preMatch)
+    test("[H1]", firstHalf)
+    test("[H2]", secondHalf)
+    test("[HT]", halfTime)
+    test("[FT]", fullTime)
+  }
+
+  test("Validate Short Period should return a Left(reason) for invalid short form") {
+    def test(p: String) = {
+      val actual = TimeConverter.validateShortPeriod(p)
+      val invalidString = s"$p is an invalid short form period"
+      assert(actual === Left(invalidString))
+    }
+
+    test("PM")
+    test("hello")
+    test("")
+    test("$%^&&*^%@")
+    test("[H1")
+    test("FOO")
+  }
+}
